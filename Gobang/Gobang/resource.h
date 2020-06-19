70816e9f3bf79bf406489a11f1876e8a2ef7d3bc
朋朋 谷 <2608114546@qq.com>
2020-06-19 17:06:56 +0800

    g3

--- Gobang/Gobang/resource.h
+++ Gobang/Gobang/resource.h
@@ -14,6 +14,7 @@
 #define IDC_BUTTON3                     1002
 #define IDC_BUTTON4                     1003
 #define IDC_BUTTON5                     1004
+#define IDC_BOARD                       1005
 
 // Next default values for new objects
 // 
@@ -21,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
